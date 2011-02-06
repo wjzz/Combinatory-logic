77ee32074ea3cbf5e16ddb7932b9d9201828b4ca
Wojtek <wjedynak@gmail.com>
2011-02-06 15:02:24 +0100

    Commented rewrite.

--- rewrite-tests.lisp
+++ rewrite-tests.lisp
@@ -9,7 +9,6 @@
 (rcomb B x y z = x (y z))
 (rcomb T x y z = x (z y))
 (rcomb S x y z = x z (y z))
-(print-def-db)
 (get-combinators *rules*)
 
 
@@ -17,13 +16,16 @@
 (unit-test (simplify-expression '((((A))))))
 (unit-test (simplify-expression '(((A B) C) D)))
 (unit-test (simplify-expression '((comp A B) C)))
+(unit-test (simplify-expression '(A ((B C) D))))
 
 
 ;; REWRITES
 
-(print-def-db)
-(rewrite '(I M))
-(rewrite '(M (M M)))
+(unit-test (get-combinators *rules*))
+(unit-test (rewrite '(I M)))
+(unit-test (rewrite '(M (M M))))
+(unit-test (rewrite '(X (I M))))
+(unit-test (rewrite '((comp I M) X))) ;; this mustn't work
 
 
 ;; (full-rewrite '(M M))
--- rewrite.lisp
+++ rewrite.lisp
@@ -24,14 +24,12 @@
 
        ; a nested application can be flattened
        ; ((A B) C) -> (A B C)
-       ; notice that if A = comp then this tranformation cannot be performed 
-       ; (in the general case)!
-       ((and (consp (first expression))
-	     (not (eql 'comp (first (first expression)))))
+       ((consp (first expression))
 	(simplify-expression (append (first expression)
 				     (rest expression))))
 
        ; apply simplify to subexpressions and check if simplify changed anything
+       ; (A ((B C) D) -> (A (B C D))
        (t 
 	(let ((result-expression (mapcar #'simplify-expression expression)))
 	  (if (equal expression result-expression)
@@ -45,45 +43,37 @@
 ;; -----------------
 
 
-(defun compositionp (expression)
-  (and (consp expression)
-       (eq 'comp (first expression))))
-
-
-;; TODO this looks hard to read
-;; It might be agood idea to introduce more predicates like compositionp
-
 (defun rewrite-step (expression rule-db)
-  "Makes a single rewrite based on the rule-db. The highest term is rewritten."
+  "Makes a single rewrite based on the rule-db. Uses the normal evaluation strategy, so the outermost combinator is expanded."
   (cond 
     ((atom expression) expression)
     ((consp expression)
      (cond
-       ((compositionp (first expression))
-	(let* ((comp-expr (first expression))
-	       ; comp-expr = ('comp outer inner)
-	       (outer     (second comp-expr))
-	       ; TODO this will work only for exactly two functions composed
-	       (inner     (third  comp-expr)))
-	  (list outer (list inner (rest expression)))))       
-	  
        ((consp (first expression))
 	      (cons (rewrite-step (first expression) rule-db)
 		    (rest expression)))
 
+       ;; If we found a combinator we want to rewrite the expression using the definition of the combinator
        ((atom (first expression))
 	(let 
 	    ((combinator (find-combinator (first expression) rule-db)))
-	  ;; If the atom is not the base we assume it's parameter variable and leave it
+
+	  ;; If the atom is not in the combinator base we assume it's a parameter variable and we don't touch it
 	  (if (null combinator)
 	      expression
 	      (let*
 		  ((parameters (combinator-parameters combinator))
 		   (split-parameters (break-at (length parameters) (rest expression)))
 		   (values       (car split-parameters)))
+
+		;; We have to check if enough parameters were provided
+		;; If not, we don't apply the combinator
 		(if (< (length values)
 		       (length parameters))
 		    expression
+		    
+		    ;; If K is a binary combinator and K x y ==> e then
+		    ;; (K x y A B ... Z) ==> (e A B ... Z)
 		    (let* 
 			((rest-of-expr (cdr split-parameters))
 			 (body         (combinator-body combinator))
@@ -94,6 +84,7 @@
 
 (defun rewrite (expression)
   (rewrite-step expression *rules*))
+
 
 
 
